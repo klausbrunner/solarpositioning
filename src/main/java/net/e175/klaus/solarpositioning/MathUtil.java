0f4d68e3c44bc791d57d5dcfb460bbcfc35cc8dc
Klaus Brunner <klaus.brunner@gmail.com>
2025-05-26 07:27:19 +0200

    chore: minor cleanups

--- src/main/java/net/e175/klaus/solarpositioning/MathUtil.java
+++ src/main/java/net/e175/klaus/solarpositioning/MathUtil.java
@@ -3,23 +3,11 @@ package net.e175.klaus.solarpositioning;
 final class MathUtil {
   private MathUtil() {}
 
-  static double polynomial(double x, double coeff0, double coeff1, double coeff2, double coeff3) {
-    return ((coeff3 * x + coeff2) * x + coeff1) * x + coeff0;
-  }
-
-  static double polynomial(double x, double coeff0, double coeff1, double coeff2) {
-    return (coeff2 * x + coeff1) * x + coeff0;
-  }
-
-  static double polynomial(double x, double coeff0, double coeff1) {
-    return coeff1 * x + coeff0;
-  }
-
   static double polynomial(double x, double... coeffs) {
     int n = coeffs.length - 1;
     double sum = coeffs[n];
     for (int i = n - 1; i >= 0; i--) {
-      sum = coeffs[i] + (x * sum);
+      sum = Math.fma(x, sum, coeffs[i]);
     }
     return sum;
   }
